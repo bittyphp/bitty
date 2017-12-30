c1986ffba8e949578308c7dea0b138b40b744e0d
bizurkur <bizurkur@gmail.com>
2017-12-30 16:52:29 -0600

    Added JsonResponse tests

--- src/Http/JsonResponse.php
+++ src/Http/JsonResponse.php
@@ -16,9 +16,12 @@ class JsonResponse extends Response
         $statusCode = 200,
         array $headers = []
     ) {
-        $headers['Content-type'] = ['application/json'];
         $json = json_encode($body);
-
         parent::__construct($json, $statusCode, $headers);
+
+        // forcibly override content type
+        $clone = $this->withHeader('Content-Type', 'application/json');
+
+        $this->headers = $clone->getHeaders();
     }
 }
