e036076257e3e38d576b260a2626b5f976850704
bizurkur <bizurkur@gmail.com>
2018-01-14 14:54:33 -0600

    Moved settings to config array

--- src/Security/Context/Context.php
+++ src/Security/Context/Context.php
@@ -18,41 +18,20 @@ class Context implements ContextInterface
     protected $paths = null;
 
     /**
-     * @var bool
+     * @var mixed[]
      */
-    protected $default = null;
-
-    /**
-     * @var int
-     */
-    protected $ttl = null;
-
-    /**
-     * @var int
-     */
-    protected $timeout = null;
-
-    /**
-     * @var int
-     */
-    protected $delay = null;
+    protected $config = null;
 
     /**
      * @param string $name
      * @param string[] $paths
-     * @param bool $default Whether or not this is the default security context.
-     * @param int $ttl Time-to-Live; how long (in seconds) authentication lasts.
-     * @param int $timeout Timeout (in seconds) due to inactivity; 0 to disable.
-     * @param int $delay Delay (in seconds) before destroy on re-authentication.
+     * @param mixed[] $config
      */
-    public function __construct($name, array $paths, $default = true, $ttl = 86400, $timeout = 0, $delay = 300)
+    public function __construct($name, array $paths, array $config = [])
     {
-        $this->name    = $name;
-        $this->paths   = $paths;
-        $this->default = (bool) $default;
-        $this->ttl     = (int) $ttl;
-        $this->timeout = (int) $timeout;
-        $this->delay   = (int) $delay;
+        $this->name   = $name;
+        $this->paths  = $paths;
+        $this->config = array_merge($this->getDefaultConfig(), $config);
     }
 
     /**
@@ -60,7 +39,7 @@ class Context implements ContextInterface
      */
     public function isDefault()
     {
-        return $this->default;
+        return (bool) $this->config['default'];
     }
 
     /**
@@ -73,12 +52,12 @@ class Context implements ContextInterface
             // TODO: Secure this more?
             // http://php.net/manual/en/features.session.security.management.php#features.session.security.management.session-id-regeneration
             // http://php.net/manual/en/function.session-regenerate-id.php
-            $this->set('destroy', $now + $this->delay);
+            $this->set('destroy', $now + $this->config['delay']);
             session_regenerate_id();
             $this->remove('destroy');
             $this->set('login', $now);
             $this->set('active', $now);
-            $this->set('expires', $now + $this->ttl);
+            $this->set('expires', $now + $this->config['ttl']);
         }
 
         $_SESSION['shield.'.$this->name][$name] = $value;
@@ -93,7 +72,7 @@ class Context implements ContextInterface
             $now     = time();
             $expires = $this->get('expires', 0);
             $destroy = $this->get('destroy', INF);
-            $active  = $this->timeout > 0 ? $this->get('active', 0) + $this->timeout : INF;
+            $active  = $this->get('active', 0) + ($this->config['timeout'] ?: INF);
             $clear   = min($expires, $destroy, $active);
 
             if ($now > $clear) {
@@ -158,5 +137,20 @@ class Context implements ContextInterface
         }
 
         return [];
+    }
+
+    /**
+     * Gets the default configuration settings.
+     *
+     * @return mixed[]
+     */
+    protected function getDefaultConfig()
+    {
+        return [
+            'default' => true,
+            'ttl' => 86400,
+            'timeout' => 0,
+            'destroy.delay' => 300,
+        ];
     }
 }
