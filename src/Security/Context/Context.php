5122c4faf74a6227c0110d0e08a7b7cb32e4a857
bizurkur <bizurkur@gmail.com>
2018-01-14 09:11:17 -0600

    Added session timeout

--- src/Security/Context/Context.php
+++ src/Security/Context/Context.php
@@ -30,6 +30,11 @@ class Context implements ContextInterface
     /**
      * @var int
      */
+    protected $timeout = null;
+
+    /**
+     * @var int
+     */
     protected $delay = null;
 
     /**
@@ -37,14 +42,16 @@ class Context implements ContextInterface
      * @param string[] $paths
      * @param bool $default Whether or not this is the default security context.
      * @param int $ttl Time-to-Live; how long (in seconds) authentication lasts.
+     * @param int $timeout Timeout (in seconds) due to inactivity; 0 to disable.
      * @param int $delay Delay (in seconds) before destroy on re-authentication.
      */
-    public function __construct($name, array $paths, $default = true, $ttl = 86400, $delay = 300)
+    public function __construct($name, array $paths, $default = true, $ttl = 86400, $timeout = 0, $delay = 300)
     {
         $this->name    = $name;
         $this->paths   = $paths;
         $this->default = (bool) $default;
         $this->ttl     = (int) $ttl;
+        $this->timeout = (int) $timeout;
         $this->delay   = (int) $delay;
     }
 
@@ -62,13 +69,16 @@ class Context implements ContextInterface
     public function set($name, $value)
     {
         if ('user' === $name) {
+            $now = time();
             // TODO: Secure this more?
             // http://php.net/manual/en/features.session.security.management.php#features.session.security.management.session-id-regeneration
             // http://php.net/manual/en/function.session-regenerate-id.php
-            $this->set('destroyed', time());
+            $this->set('destroy', $now + $this->delay);
             session_regenerate_id();
-            $this->remove('destroyed');
-            $this->set('expires', time() + $this->ttl);
+            $this->remove('destroy');
+            $this->set('login', $now);
+            $this->set('active', $now);
+            $this->set('expires', $now + $this->ttl);
         }
 
         $_SESSION['shield.'.$this->name][$name] = $value;
@@ -80,19 +90,19 @@ class Context implements ContextInterface
     public function get($name, $default = null)
     {
         if ('user' === $name) {
-            $expires = $this->get('expires');
-            if ($expires && time() > $expires) {
-                // This session has timed out.
-                // Clear out all data to prevent unauthorized use.
-                $this->clear();
-            }
+            $now     = time();
+            $expires = $this->get('expires', 0);
+            $destroy = $this->get('destroy', INF);
+            $active  = $this->timeout > 0 ? $this->get('active', 0) + $this->timeout : INF;
+            $clear   = min($expires, $destroy, $active);
 
-            $destroyed = $this->get('destroyed', null);
-            if ($destroyed && time() > $destroyed + $this->delay) {
-                // This session has been destroyed.
+            if ($now > $clear) {
+                // This session should be destroyed.
                 // Clear out all data to prevent unauthorized use.
-                // TODO: Trigger alarm/log/event?
                 $this->clear();
+            } else {
+                // Update last active time.
+                $this->set('active', $now);
             }
         }
 
